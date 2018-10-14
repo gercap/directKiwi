40fad374399a18f53481ce55094da7e2bd1231dc
gercap <germanocapela@gmail.com>
2018-10-14 15:37:56 +0100

    external script for listening to the audio stream

---
+++ sub_client_audio.py
@@ -0,0 +1,31 @@
+import sys, platform, struct
+import zmq
+import numpy, pygame
+
+port = "5556"
+if len(sys.argv) > 1:
+    port =  sys.argv[1]
+    int(port)
+
+# Socket to talk to server
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+
+print "Collecting data from server..."
+socket.connect ("tcp://localhost:%s" % port)
+
+topicfilter = ""
+socket.setsockopt(zmq.SUBSCRIBE, topicfilter)
+
+while True:
+    body = socket.recv()
+
+    seq = struct.unpack('<I', body[0:4])[0]
+    smeter = struct.unpack('>H', body[4:6])[0]
+    rssi = (smeter & 0x0FFF) // 10 - 127
+
+    messagedata = numpy.fromstring(body[6:], numpy.int16)
+    pygame.mixer.Channel(0).queue(pygame.sndarray.make_sound(numpy.array(messagedata, numpy.int16)))
+
+    sys.stdout.write('\r Sample Size: %-04d Block: %08x, RSSI: %-04d' % (len(messagedata), seq, rssi))
+    sys.stdout.flush()
